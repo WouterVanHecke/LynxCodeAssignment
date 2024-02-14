41ae5672a3355d036cd630d378c4adba246a4b24
WouterVH <wouter.190@hotmail.com>
2024-02-14 20:08:59 +0100

    :sparkles: Create form

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
+import FieldInput from "@/components/FieldInput";
+import React, { useCallback, useState } from "react";
 
 /**
  * We need:
@@ -12,15 +13,72 @@ import React, { useState } from "react";
  */
 
 const Account = () => {
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [licensePlate, setLicensePlate] = useState();
-  const [brand, setBrand] = useState();
-  const [model, setModel] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [licensePlate, setLicensePlate] = useState("");
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+
+  const submitAccountInfo = useCallback(() => {}, []);
+
+  const handleFirstNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(event.target.value);
+  }, []);
+
+  const handleLastNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(event.target.value);
+  }, []);
+
+  const handleLicensePlateNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setLicensePlate(event.target.value);
+  }, []);
+
+  const handleBrandNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setBrand(event.target.value);
+  }, []);
+
+  const handleModelNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    setModel(event.target.value);
+  }, []);
 
   return (
-    <div className="w-screen h-screen bg-black flex items-center justify-center">
-      <p className="text-white text-4xl">Create account</p>
+    <div className="w-screen h-screen bg-black flex items-center justify-center flex-col">
+      <p className="text-white text-4xl mb-8">Create account</p>
+      <form action={submitAccountInfo}>
+        <div className="mb-6 flex">
+          <FieldInput
+            label={"firstName"}
+            value={firstName}
+            handleValueChange={handleFirstNameChange}
+          />
+
+          <FieldInput
+            label={"lastName"}
+            value={lastName}
+            handleValueChange={handleLastNameChange}
+          />
+        </div>
+        <div className="mb-6 flex">
+          <FieldInput label={"brand"} value={brand} handleValueChange={handleBrandNameChange} />
+
+          <FieldInput label={"model"} value={model} handleValueChange={handleModelNameChange} />
+        </div>
+        <div className="flex justify-center">
+          <FieldInput
+            label={"license plate"}
+            value={licensePlate}
+            handleValueChange={handleLicensePlateNameChange}
+          />
+          <div className="w-full flex justify-center items-end">
+            <button
+              type="submit"
+              className="text-white bg-blue-500 rounded p-2 px-6 cursor-pointer"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
